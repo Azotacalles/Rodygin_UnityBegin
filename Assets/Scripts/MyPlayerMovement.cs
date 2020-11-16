82bb6584500190b6c16484c6d9024bfcd514aedf
Pavel <paha-r@yandex.ru>
2020-11-16 18:33:57 +0300

    Rotation
    Не всегда сразу поворачивает

--- Assets/Scripts/MyPlayerMovement.cs
+++ Assets/Scripts/MyPlayerMovement.cs
@@ -6,19 +6,27 @@ public class MyPlayerMovement : MonoBehaviour
 {
     [SerializeField] private float jump;
     [SerializeField] private float speed;
+    [SerializeField] private float turnSpeed;
     private Vector3 direction;
     private Rigidbody rb;
+
+    private Vector3 rotation;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     private void Update()
     {
-        direction.x = Input.GetAxis("Horizontal");
+        //direction.x = Input.GetAxis("Horizontal");
+        rotation.y = Input.GetAxis("Horizontal");
         direction.z = Input.GetAxis("Vertical");
+        if(Input.GetButtonDown("Vertical") && direction.z < 0)
+        {
+            transform.rotation *= Quaternion.AngleAxis(180, transform.up);
+            print("---");
+        }
         if (Input.GetKeyDown(KeyCode.Space))
             rb.AddForce(Vector3.up * jump, ForceMode.Force);
     }
@@ -27,5 +35,7 @@ public class MyPlayerMovement : MonoBehaviour
     {
         var newSpeed = direction * speed * Time.fixedDeltaTime;
         transform.Translate(newSpeed);
+
+        transform.Rotate(rotation * turnSpeed * Time.fixedDeltaTime);
     }
 }
